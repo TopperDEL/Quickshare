6b268a009dcd137b8307d593ad6eed923ea6ac53
TopperDEL <github_com@tparth.de>
2022-08-11 09:27:33 +0200

    fix: No error on correct upload

--- Quickshare/Program.cs
+++ Quickshare/Program.cs
@@ -24,11 +24,20 @@ string configLocation = Path.Combine(appData, @"\quickshare.config");
 
 //Check if the user wants to set new config-values
 bool requestsConfigUpdate = false;
+bool noValidOptions = false;
 Parser.Default.ParseArguments<CommandLineConfigOptions, CommandLineShareOptions>(args)
         .WithParsed<CommandLineConfigOptions>(o =>
         {
             requestsConfigUpdate = true;
+        })
+        .WithNotParsed(o=>{
+            noValidOptions = true;
         });
+
+if(noValidOptions)
+{
+    return;
+}
 
 if (!File.Exists(configLocation) || requestsConfigUpdate)
 {
